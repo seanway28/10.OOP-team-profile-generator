c12f559bb00ec32552328d0985df781834e2122d
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-17 11:24:40 -0400

    npm rework

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,8 +1,65 @@
-const checkIfEqual = require('../lib/random.js');
+const Manager = require ('../lib/Manager');
 
-test('checks if 10 is equal to 10', () => {
-  expect(checkIfEqual(10, 10)).toBe(true);
+// Test for manager object
+test ('creates a manager object', () => {
+  const manager = new Manager('Dan', 2, 'Dan28@icloud.com');
+
+  expect(manager.name).toBe('Dan');
+  expect(manager.id).toEqual(expect.any(Number));
+  expect(manager.email).toEqual(expect.stingContaining('@'));
 });
+//Test for Name
+test ('gets name of manager', () => {
+    const manager = new Manager('Dan', 2, 'Dan28@icloud.com');
+
+    expect(manager.getName()).toBe('Eric');
+});
+// Test for id
+test ('gets id of manager', () => {
+    const manager = new Manager('Dan', 2, 'Dan28@icloud.com');
+
+    expect(manager.getId()).toBe(2);
+});
+// Test for email
+test ('gets email of manager', () => {
+    const manager = new Manager('Dan', 2, 'Dan28@icloud.com');
+
+    expect(manager.getEmail()).toEqual(expect.stringContaining('@'));
+});
+// Test for role
+test ('gets role of manager', () => {
+    const manager = new Manager('Dan', 2, 'Dan28@icloud.com');
+
+    expect(manager.getRole()).toEqual('Manager');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const checkIfEqual = require('../lib/random.js');
+
+// test('checks if 10 is equal to 10', () => {
+//   expect(checkIfEqual(10, 10)).toBe(true);
+// });
 
 // function Potion(name) {
 //   this.types = ['strength', 'agility', 'health'];
